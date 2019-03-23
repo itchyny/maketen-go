601ddff9321dadb4085124ecceb269d54af9a48a
itchyny <itchyny@hatena.ne.jp>
2019-03-23 17:03:22 +0900

    remove duplicates according to string representation

--- solve.go
+++ solve.go
@@ -3,13 +3,18 @@ package maketen
 // Solve maketen.
 func Solve(x, y, z, w Num) []Expr {
 	var exprs []Expr
+	res := make(map[string]struct{})
 	ten := NewInt(10)
 	for _, a := range operators {
 		for _, b := range operators {
 			for _, c := range operators {
 				for _, t := range trees(x, y, z, w, a, b, c) {
 					if e := Eval(t); e != nil && e.Cmp(ten) == 0 {
-						exprs = append(exprs, t)
+						s := t.String()
+						if _, ok := res[s]; !ok {
+							res[s] = struct{}{}
+							exprs = append(exprs, t)
+						}
 					}
 				}
 			}
